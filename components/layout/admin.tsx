146218ebfbc9d1d3cf0bcc41b3ae26629f74ebfd
Lê Thanh Bình <lethanhbinh26062002@gmail.com>
2022-07-24 18:06:22 +0700

    AN-24 <UI admin/user>

--- components/layout/admin.tsx
+++ components/layout/admin.tsx
@@ -1,35 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
 import { LayoutProps } from '../../models/layout'
 
 type Props = {}
 
-const AdminLayout = ({children}: LayoutProps) => {
+const AdminLayout = ({ children }: LayoutProps) => {
     return (
         <div>
             <header className="fixed right-0 top-0 left-60 bg-yellow-50 py-3 px-4 h-16">
                 <div className="max-w-4xl mx-auto">
                     <div className="flex items-center justify-between">
-                        <div>
-                            <button type="button" className="flex items-center focus:outline-none rounded-lg text-gray-600 hover:text-yellow-600 focus:text-yellow-600 font-semibold p-2 border border-transparent hover:border-yellow-300 focus:border-yellow-300 transition">
-                                <span className="inline-flex items-center justify-center w-6 h-6 text-gray-600 text-xs rounded bg-white transition mr-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="bi bi-chevron-left" viewBox="0 0 16 16">
-                                        <path fillRule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
-                                    </svg>
-                                </span>
-                                <span className="text-sm">Archive</span>
-                            </button>
-                        </div>
                         <div className="text-lg font-bold">Todays Plan</div>
-                        <div>
-                            <button type="button" className="flex items-center focus:outline-none rounded-lg text-gray-600 hover:text-yellow-600 focus:text-yellow-600 font-semibold p-2 border border-transparent hover:border-yellow-300 focus:border-yellow-300 transition">
-                                <span className="text-sm">This week</span>
-                                <span className="inline-flex items-center justify-center w-6 h-6 text-gray-600 text-xs rounded bg-white transition ml-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="bi bi-chevron-right" viewBox="0 0 16 16">
-                                        <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
-                                    </svg>
-                                </span>
-                            </button>
-                        </div>
                     </div>
                 </div>
             </header>
@@ -42,39 +23,49 @@ const AdminLayout = ({children}: LayoutProps) => {
                         <div className="p-4">
                             <ul className="space-y-1">
                                 <li>
-                                    <a href="javascript:void(0)" className="flex items-center bg-yellow-200 rounded-xl font-bold text-sm text-yellow-900 py-3 px-4">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="text-lg mr-4" viewBox="0 0 16 16">
-                                            <path d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4V.5zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2zm-3.5-7h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5z" />
-                                        </svg>Dashboards
-                                    </a>
+                                    <Link href="/admin">
+                                        <a href="javascript:void(0)" className="flex items-center bg-yellow-200 rounded-xl font-bold text-sm text-yellow-900 py-3 px-4">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="text-lg mr-4" viewBox="0 0 16 16">
+                                                <path d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4V.5zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2zm-3.5-7h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5z" />
+                                            </svg>Dashboards
+                                        </a>
+                                    </Link>
                                 </li>
                                 <li>
-                                    <a href="javascript:void(0)" className="flex bg-white hover:bg-yellow-50 rounded-xl font-bold text-sm text-gray-900 py-3 px-4">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="text-lg mr-4" viewBox="0 0 16 16">
-                                            <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM5 4h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1zm0 2h3a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1z" />
-                                        </svg>Orders
-                                    </a>
+                                    <Link href="/admin/orders">
+                                        <a href="javascript:void(0)" className="flex bg-white hover:bg-yellow-50 rounded-xl font-bold text-sm text-gray-900 py-3 px-4">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="text-lg mr-4" viewBox="0 0 16 16">
+                                                <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM5 4h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1zm0 2h3a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1z" />
+                                            </svg>Orders
+                                        </a>
+                                    </Link>
                                 </li>
                                 <li>
-                                    <a href="javascript:void(0)" className="flex bg-white hover:bg-yellow-50 rounded-xl font-bold text-sm text-gray-900 py-3 px-4">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="text-lg mr-4" viewBox="0 0 16 16">
-                                            <path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3zm-8.322.12C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139z" />
-                                        </svg>Products
-                                    </a>
+                                    <Link href="/admin/products">
+                                        <a href="javascript:void(0)" className="flex bg-white hover:bg-yellow-50 rounded-xl font-bold text-sm text-gray-900 py-3 px-4">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="text-lg mr-4" viewBox="0 0 16 16">
+                                                <path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3zm-8.322.12C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139z" />
+                                            </svg>Products
+                                        </a>
+                                    </Link>
                                 </li>
                                 <li>
-                                    <a href="javascript:void(0)" className="flex bg-white hover:bg-yellow-50 rounded-xl font-bold text-sm text-gray-900 py-3 px-4">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="text-lg mr-4" viewBox="0 0 16 16">
-                                            <path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3zm-8.322.12C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139z" />
-                                        </svg>Categories
-                                    </a>
+                                    <Link href="/admin/categories">
+                                        <a href="javascript:void(0)" className="flex bg-white hover:bg-yellow-50 rounded-xl font-bold text-sm text-gray-900 py-3 px-4">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="text-lg mr-4" viewBox="0 0 16 16">
+                                                <path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3zm-8.322.12C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139z" />
+                                            </svg>Categories
+                                        </a>
+                                    </Link>
                                 </li>
                                 <li>
-                                    <a href="javascript:void(0)" className="flex bg-white hover:bg-yellow-50 rounded-xl font-bold text-sm text-gray-900 py-3 px-4">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} fill="currentColor" className="text-lg mr-4" viewBox="0 0 16 16">
-                                            <path d="M2 1a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l4.586-4.586a1 1 0 0 0 0-1.414l-7-7A1 1 0 0 0 6.586 1H2zm4 3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
-                                        </svg>Users
-                                    </a>
+                                    <Link href="/admin/users">
+                                        <a href="javascript:void(0)" className="flex bg-white hover:bg-yellow-50 rounded-xl font-bold text-sm text-gray-900 py-3 px-4">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} fill="currentColor" className="text-lg mr-4" viewBox="0 0 16 16">
+                                                <path d="M2 1a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l4.586-4.586a1 1 0 0 0 0-1.414l-7-7A1 1 0 0 0 6.586 1H2zm4 3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                                            </svg>Users
+                                        </a>
+                                    </Link>
                                 </li>
                             </ul>
                         </div>
---
+++ pages/admin/users/index.tsx
@@ -0,0 +1,96 @@
+import Link from 'next/link'
+import React from 'react'
+import AdminLayout from '../../../components/layout/admin'
+
+type Props = {}
+
+const Users = (props: Props) => {
+    return (
+        <div className="">
+            <AdminLayout> </AdminLayout>
+            <main className="ml-60 pt-16 max-h-screen overflow-auto">
+                <div className="px-6 py-8">
+                    <div className="max-w-4xl mx-auto">
+                        <div className="overflow-x-auto relative shadow-md sm:rounded-lg ">
+
+                            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                    <tr>
+                                        <th scope="col" className="p-4">
+                                            ID
+                                        </th>
+                                        <th scope="col" className="py-3 px-6">
+                                            Name
+                                        </th>
+                                        <th scope="col" className="py-3 px-6">
+                                            Phone
+                                        </th>
+                                        <th scope="col" className="py-3 px-6">
+                                            Status
+                                        </th>
+                                        <th scope="col" className="py-3 px-6">
+                                            Action
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                        <td className="p-4 w-4">
+                                            1
+                                        </td>
+                                        <th scope="row" className="flex items-center py-4 px-6 text-gray-900 whitespace-nowrap dark:text-white">
+                                            <img className="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-1.jpg" />
+                                            <div className="pl-3">
+                                                <div className="text-base font-semibold">Neil Sims</div>
+                                                <div className="font-normal text-gray-500">neil.sims@flowbite.com</div>
+                                            </div>
+                                        </th>
+                                        <td className="py-4 px-6">
+                                            React Developer
+                                        </td>
+                                        <td className="py-4 px-6">
+                                            <div className="flex items-center">
+                                                <div className="h-2.5 w-2.5 rounded-full bg-green-400 mr-2" /> ON
+                                            </div>
+                                        </td>
+                                        <td className="py-4 px-6">
+                                            <Link href="users/edit"><button className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</button></Link>
+                                            <button className="font-medium text-red-600 dark:text-red-500 hover:underline ml-5">Delete </button>
+                                        </td>
+                                    </tr>
+                                    <tr className="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                        <td className="p-4 w-4">
+                                            5
+                                        </td>
+                                        <th scope="row" className="flex items-center py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            <img className="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-4.jpg" />
+                                            <div className="pl-3">
+                                                <div className="text-base font-semibold">Leslie Livingston</div>
+                                                <div className="font-normal text-gray-500">leslie@flowbite.com</div>
+                                            </div>
+                                        </th>
+                                        <td className="py-4 px-6">
+                                            SEO Specialist
+                                        </td>
+                                        <td className="py-4 px-6">
+                                            <div className="flex items-center">
+                                                <div className="h-2.5 w-2.5 rounded-full bg-red-500 mr-2" /> OFF
+                                            </div>
+                                        </td>
+                                        <td className="py-4 px-6">
+                                            <Link href="users/edit"><button className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</button></Link>
+                                            <button className="font-medium text-red-600 dark:text-red-500 hover:underline ml-5">Delete </button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </main>
+
+        </div >
+    )
+}
+
+export default Users
