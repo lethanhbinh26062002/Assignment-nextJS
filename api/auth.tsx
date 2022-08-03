a8e8899fedacde300f6d7b9c581d8f012b9dd278
thamcthph16766 <caohongtham02@gmail.com>
2022-08-03 23:36:58 +0700

    AN-49 "login"

--- api/auth.tsx
+++ api/auth.tsx
@@ -4,3 +4,6 @@ import {registerType} from "../models/register"
 export const signup = (user: registerType) => {
     return instance.post("/signup",user);
 }
+export const signin = (user: registerType) => {
+    return instance.post("/login",user);
+}
--- hooks/useRegister.ts
+++ hooks/useRegister.ts
@@ -1,28 +1,8 @@
 import useSWR, { useSWRConfig } from "swr";
-import { signup } from "../api/auth";
+import { signup, signin } from "../api/auth";
 import { registerType } from "../models/register";
 
-// export const useRegister = () => {
-//     // const url = "/users"
-//     // const { data, error, mutate } = useSWR(url)
-//     // const signup = async(user : registerType) =>{
-//     //     const item = await signup(user);
-//     //     mutate([...data,item])
-//     // };
-//     const register = (user) => {
-//         const url = "/users"
-//         const { data, error, mutate } = useSWR(url)
-//         mutate("/users", async (user: registerType) => {
-//             const user = await signup(user);
-//             return [...data, user];
-//         });
-//     };
 export const useRegister = () => {
-    // get list user
-    // const fetcher = async (url: string) => {
-    //     const { data } = await list(url);
-    //     return data;
-    // };
 
     const { data, error } = useSWR("/signup");
     const { mutate } = useSWRConfig();
@@ -34,9 +14,17 @@ export const useRegister = () => {
             return [...data, user];
         });
     };
+    const login = (user: registerType) => {
+        const account = user;
+        mutate("/login", async () => {
+            const { data: user } = await signin(account);
+            return [...data, user];
+        });
+    };
     return {
         data,
         error,
         signup,
+        signin,
     }
 }
