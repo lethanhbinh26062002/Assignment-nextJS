488d930df5264573a8dd7ef2e9680238781f04b3
thamcthph16766 <caohongtham02@gmail.com>
2022-08-05 00:04:34 +0700

    AN-56 <users>

--- pages/admin/users/edit/[id].tsx
+++ pages/admin/users/edit/[id].tsx
@@ -1,9 +1,28 @@
+import { useRouter } from 'next/router';
 import React from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form';
+import useSWR from 'swr';
 import AdminLayout from '../../../../components/layout/admin'
+import { useUser } from '../../../../hooks/useUser';
+import { userType } from '../../../../models/userType';
 
-type Props = {}
+type Props = {
+}
 
 const EditUser = (props: Props) => {
+    const { register, formState: errors, handleSubmit } = useForm();
+    const router = useRouter();
+    const { updateUser } = useUser();
+    const {id}  = router.query
+    const { data, error } = useSWR(`/users/${id}`);
+    
+    const onSubmit: SubmitHandler<userType> = async (data:userType) => {
+        await updateUser(data);
+        router.push('/admin/users')
+        
+    }
+    if (!data) return <div>Loading...</div>
+    if (error) return <div>Error : {error}</div>
     return (
         <div>
             <AdminLayout> </AdminLayout>
@@ -14,29 +33,25 @@ const EditUser = (props: Props) => {
                             <div>
                                 <section className="max-w-4xl p-6 mx-auto bg-indigo-600 rounded-md shadow-md dark:bg-gray-800 mt-20">
                                     <h1 className="text-xl font-bold text-white capitalize dark:text-white">Edit User </h1>
-                                    <form>
+                                    <form onSubmit={handleSubmit(onSubmit)}>
                                         <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
                                             <div>
                                                 <label className="text-white dark:text-gray-200" htmlFor="username">Username</label>
-                                                <input id="username" type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" />
+                                                <input {...register('name', { required: true })} type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" />
                                             </div>
                                             <div>
                                                 <label className="text-white dark:text-gray-200" htmlFor="emailAddress">Email Address</label>
-                                                <input id="emailAddress" type="email" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" />
+                                                <input {...register('email', { required: true })} type="email" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" />
                                             </div>
                                             <div>
                                                 <label className="text-white dark:text-gray-200" htmlFor="password">Password</label>
-                                                <input id="password" type="password" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" />
-                                            </div>
-                                            <div>
-                                                <label className="text-white dark:text-gray-200" htmlFor="username">Phone Number</label>
-                                                <input id="username" type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" />
+                                                <input {...register('password', { required: true })} type="password" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" />
                                             </div>
                                             <div>
                                                 <label className="text-white dark:text-gray-200" htmlFor="username">Address</label>
-                                                <input id="address" type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" />
+                                                <input {...register('address', { required: true })} type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" />
                                             </div>
-                                            <div>
+                                            {/* <div>
                                                 <label className="block text-sm font-medium text-white">
                                                     Image
                                                 </label>
@@ -57,7 +72,7 @@ const EditUser = (props: Props) => {
                                                         </p>
                                                     </div>
                                                 </div>
-                                            </div>
+                                            </div> */}
                                         </div>
                                         <div className="flex justify-end mt-6">
                                             <button className="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-pink-500 rounded-md hover:bg-pink-700 focus:outline-none focus:bg-gray-600">Update</button>
