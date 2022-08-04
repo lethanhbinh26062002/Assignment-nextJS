f68bb8411b15706c3a44831cbd40d1649e84fa78
thamcthph16766 <caohongtham02@gmail.com>
2022-08-04 14:28:07 +0700

    add user

--- hooks/useRegister.ts
+++ hooks/useRegister.ts
@@ -1,28 +1,7 @@
 import useSWR, { useSWRConfig } from "swr";
 import { signup } from "../api/auth";
 import { registerType } from "../models/register";
-
-// export const useRegister = () => {
-//     // const url = "/users"
-//     // const { data, error, mutate } = useSWR(url)
-//     // const signup = async(user : registerType) =>{
-//     //     const item = await signup(user);
-//     //     mutate([...data,item])
-//     // };
-//     const register = (user) => {
-//         const url = "/users"
-//         const { data, error, mutate } = useSWR(url)
-//         mutate("/users", async (user: registerType) => {
-//             const user = await signup(user);
-//             return [...data, user];
-//         });
-//     };
 export const useRegister = () => {
-    // get list user
-    // const fetcher = async (url: string) => {
-    //     const { data } = await list(url);
-    //     return data;
-    // };
 
     const { data, error } = useSWR("/signup");
     const { mutate } = useSWRConfig();
