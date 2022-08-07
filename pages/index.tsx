fd0d1f33dd3df6625f3012a12ffb161a592863ad
thamcthph16766 <caohongtham02@gmail.com>
2022-08-08 00:09:31 +0700

    update home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
 import type { NextPage } from 'next'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import Main from '../components/Main'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Header></Header>
-      <main className="my-20">
-
-      </main>
+      <Main></Main>
       <Footer></Footer>
     </div>
   )
